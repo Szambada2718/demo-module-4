66f94ed2a475f9163a0945968f33b6362146b3c7
project 1 <radhityo.kusumo@gmail.com>
2024-08-13 19:06:47 +0700

    done 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import Login from "./Login";
+import GreetingRevou from "./GreetingRevou";
+
 
 function App() {
+  const name ="revou berlin";
   return (
+    // wajib dibungkus 1 element
     <div className="App">
-      {/* <div className="container">
-        <h1 className="title">Hello, React!</h1>
+        <GreetingRevou name={name} />
+        <div className="container">
+        <h1 className="title">Hello, React!{name}</h1>
         <button className="button">Click Me</button>
-      </div> */}
+      </div>
       <Login />
+
     </div>
   );
 }
---
+++ src/GreetingRevou.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface GreetingProps {
+    name: string
+}
+
+const GreetingRevou = ({name}: 
+GreetingProps) => {  
+    return (
+        <div>
+            <h1>Greeting component revou berlin
+
+            </h1>
+        </div>
+    );
+};
+
+export default GreetingRevou;
